6443c7bc32016ed521a976998e31a666fb310b39
Ignacio Ibaigorria <ignacio.ibaigorria@gmail.com>
2025-04-11 11:46:29 -0300

    -

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Desactivar ESLint durante la compilación
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
